7bccde5d4ffe5cdc75b8b251d1cda18979bd9d7b
Ruslan Ziganshin <rusiano@MacBook-Air-Ruslan.local>
2023-02-01 18:00:30 +0300

    add example_app_architecture

---
+++ src/example_app_architecture/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ExampleAppArchitectureConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'example_app_architecture'
